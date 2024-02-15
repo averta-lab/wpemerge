2e1b727ed1873cd90a69820a7a52c415217871e3
Majid <mj_azani@hotmail.com>
2024-02-15 19:39:42 +1030

    bumping requirements

--- src/Responses/ResponseService.php
+++ src/Responses/ResponseService.php
@@ -9,7 +9,7 @@
 
 namespace WPEmerge\Responses;
 
-use GuzzleHttp\Psr7;
+use GuzzleHttp\Psr7\Utils;
 use GuzzleHttp\Psr7\Response as Psr7Response;
 use WPEmerge\Requests\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -197,7 +197,7 @@ class ResponseService {
 	 */
 	public function output( $output ) {
 		$response = $this->response();
-		$response = $response->withBody( Psr7\stream_for( $output ) );
+		$response = $response->withBody( Utils::streamFor( $output ) );
 		return $response;
 	}
 
@@ -210,7 +210,7 @@ class ResponseService {
 	public function json( $data ) {
 		$response = $this->response();
 		$response = $response->withHeader( 'Content-Type', 'application/json' );
-		$response = $response->withBody( Psr7\stream_for( wp_json_encode( $data ) ) );
+		$response = $response->withBody( Utils::streamFor( wp_json_encode( $data ) ) );
 		return $response;
 	}
 
